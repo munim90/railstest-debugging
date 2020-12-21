9db06e86258bb7b27418506cd967fd4772f606f2
munim90 <munim.iftikhar@educative.io>
2020-12-21 18:15:34 +0500

    test

---
+++ spec/controllers/projects_controller_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe ProjectsController, type: :controller do
+    describe "create" do
+        it "calls the workflow with parameters" do
+            workflow = instance_spy(CreatesProject, success?: true)
+            allow(CreatesProject).to receive(:new).and_return(workflow)
+            Chapter 7. Using Test Doubles as Mocks and Stubs • 142
+            report erratum • discuss
+            post :create,
+            params: {project: {name: "Runway", tasks: "start something:2"}}
+            expect(CreatesProject).to have_received(:new)
+            .with(name: "Runway", task_string: "start something:2")
+        end
+    end
+end
