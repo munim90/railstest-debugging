b0db47b49d786b2969f0f7cb78770d1334275425
munim90 <munim.iftikhar@educative.io>
2021-01-18 11:59:35 +0500

    changee

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -1,5 +1,11 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const environment = require("./environment")
 
-const environment = require('./environment')
+environment.plugins.get("Manifest")
 
-module.exports = environment.toWebpackConfig()
+//.opts.writeToFileEmit= process.env.NODE_ENV !== "test"
+
+const config = environment.toWebpackConfig()
+
+config.devtool = "inline-source-map"
+
+module.exports = config
