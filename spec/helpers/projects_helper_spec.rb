f356c3469f737f2d3941aa2f541227df618e4ef9
munim90 <munim.iftikhar@educative.io>
2021-02-01 15:26:30 +0500

    passing testt

--- spec/helpers/projects_helper_spec.rb
+++ spec/helpers/projects_helper_spec.rb
@@ -8,5 +8,13 @@ RSpec.describe ProjectsHelper, type: :helper do
     actual = helper.name_with_status(project)
     expect(actual).to have_selector("span.on_schedule", text: "Project Runway")
   end
-  
+  #START: second test
+  it "augments with status info when behind schedule" do
+    allow(project).to receive(:on_schedule?).and_return(false)
+    actual = helper.name_with_status(project)
+    expect(actual).to have_selector(
+    "span.behind_schedule", text: "Project Runway")
+  end
+  #END: second test
+
 end
