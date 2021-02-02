c89b677d6b709357c8f19887898342b62e4cc69c
munim90 <munim.iftikhar@educative.io>
2021-02-02 20:11:47 +0500

    ttest

--- test/models/project_with_stubs_test.rb
+++ test/models/project_with_stubs_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 class ProjectWithStubsTest < ActiveSupport::TestCase
+    #START: first_stub
     test "let's stub an object" do
         project = Project.new(name: "Project Greenlight")
         project.stubs(:name)
         assert_nil(project.name)
     end
+    #END: first_stub
+
+    #START: second_stub
+    test "let's stub an object again" do
+        project = Project.new(name: "Project Greenlight")
+        project.stubs(:name).returns("Fred")
+        assert_equal("Fred", project.name)
+    end
+    #END: second_stub
 end
