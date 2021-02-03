786070f95627847f553560ecc4255a4a9757672c
munim90 <munim.iftikhar@educative.io>
2021-02-03 13:17:11 +0500

    system testtt

--- test/system/tasks_test.rb
+++ test/system/tasks_test.rb
@@ -3,12 +3,12 @@ require "application_system_test_case"
 class TasksTest < ApplicationSystemTestCase
     test "reordering a task" do
         project = FactoryBot.create(:project, name: "Project Bluebook")
-        FactoryBot.create(
+        task_1 = FactoryBot.create(
             :task, project: project, title: "Search Sky", size: 1, project_order: 1)
-        FactoryBot.create(
+        task_2 = FactoryBot.create(
             :task, project: project, title: "Use Telescope", size: 1,
                 project_order: 2)
-        FactoryBot.create(
+        task_3 = FactoryBot.create(
             :task, project: project, title: "Take Notes", size: 1,
                 project_order: 3)
         visit(project_path(project))
