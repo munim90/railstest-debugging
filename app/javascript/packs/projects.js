aa140379dea6a74ab809d98a771ef18f3dd2db0b
munim90 <munim.iftikhar@educative.io>
2021-06-15 10:15:26 +0500

    Updated code

---
+++ app/javascript/packs/projects.js
@@ -0,0 +1,13 @@
+import {Project} from "../../../app/javascript/packs/project.js"
+import {ProjectTable} from "../../../app/javascript/packs/project_table.js"
+
+$(() => {
+  if ($(".task-table").length > 0) {
+    const element = $(".task-table")
+    const project = new Project(element.data("project"))
+    project.load().then(() => {
+      const table = new ProjectTable(project, ".task-table")
+      table.insert()
+    })
+  }
+})
